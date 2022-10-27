04632b4dd27658423d474cf5d7fc7a53c16a8a52
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-27 13:17:53 -0700

    Passes test four, doubling every other digit from right to left
    Co-authored by: Yodel Guanzon <yodelguanzon@gmail.com>
    dquote> Co-authored-by: Alejandro Socarras <apsocarras@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,18 @@
 
 
 function validateCC(number) {
-
+  // Split input by spaces, join to get rid of them, split again to make each number a single element
   const numNoSpace = number.split(" ").join("").split("");
   console.log(numNoSpace);
   console.log(numNoSpace.length);
-  let ctr = 0;
-  for (i=(numNoSpace.length - 1); i >= 0; i--) { 
-    if (ctr % 2 === 0) {
-      let doubled = parseInt(numNoSpace[i]) * 2;
-      if(doubled>=10) {
-        let doubledSum = parseInt(doubled.toString()[0]) + parseInt(doubled.toString()[1]);
-        numNoSpace[i] = doubledSum;
-      } else {
-      numNoSpace[i] = doubled;
-      }; 
-    } else {
-      numNoSpace[i] = parseInt(numNoSpace[i]);
-    }
-    ctr++;
-    
-  return numNoSpace;
-  } 
+  // Iterate over array from right to left, incrementing by -2
+  for (i=(numNoSpace.length - 1); i >= 0; i-=2) { 
+    // Double every other number
+    let doubled = parseInt(numNoSpace[i]) * 2;
+    if(doubled>=10) {
+      doubled = parseInt(doubled.toString()[0]) + parseInt(doubled.toString()[1]);
+    };
+    numNoSpace[i] = doubled;
 
-}
+  return numNoSpace;
+} 
