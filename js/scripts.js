13313d9b0ed10a8c50d6eabf6b10d05a6267afbd
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-27 12:08:03 -0700

    WIP: working on test four, replacing every other digit from right to left
    Co-authored by: Yodel Guanzon <yodelguanzon@gmail.com>
    dquote> Co-authored-by: Alejandro Socarras <apsocarras@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,25 @@
 
 function validateCC(number) {
 
-  let output = [];
-  const numNoSpace = number.split(" ").join("");
+  const numNoSpace = number.split(" ").join("").split("");
   console.log(numNoSpace);
   console.log(numNoSpace.length);
-  for (i=(numNoSpace.length - 1); i >= 0; i-=2) {
-    let doubled = parseInt(numNoSpace[i]*2);
-    if(doubled>=10) {
-      doubled = parseInt(doubled.toString()[0]) + parseInt(doubled.toString()[1]);
+  let ctr = 0;
+  for (i=(numNoSpace.length - 1); i >= 0; i--) { 
+    if (ctr % 2 === 0) {
+      let doubled = parseInt(numNoSpace[i]) * 2;
+      if(doubled>=10) {
+        let doubledSum = parseInt(doubled.toString()[0]) + parseInt(doubled.toString()[1]);
+        numNoSpace[i] = doubledSum;
+      } else {
+      numNoSpace[i] = doubled;
+      }; 
+    } else {
+      numNoSpace[i] = parseInt(numNoSpace[i]);
     }
-    output.push(doubled);
+    ctr++;
+    
+  return numNoSpace;
   } 
-  return output;
+
 }
